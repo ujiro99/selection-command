877ffb156cd70abc74f58966afb8f6b94938be0d
ujiro99 <siro.cola@gmail.com>
2024-01-06 16:27:51 +0900

    Add settings that enables css.

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,13 +1,51 @@
-import * as mv3 from 'mv3-hot-reload';
-mv3.content.init();
+import * as mv3 from 'mv3-hot-reload'
+mv3.content.init()
+import * as React from 'react'
+const useState = React.useState
+import { createRoot } from 'react-dom/client'
+import { Popover } from '@headlessui/react'
+import { usePopper } from 'react-popper'
 
-import * as React from 'react';
-import { createRoot } from 'react-dom/client';
+import { popup } from './app.module.css'
 
-const rootDom = document.createElement('div');
-(rootDom as any).id = 'sample';
-(rootDom as any).style = 'z-index: 1000000';
-document.body.insertBefore(rootDom, document.body.firstChild);
+const rootDom = document.createElement('div')
+rootDom.id = 'selection-popup'
+document.body.append(rootDom)
+const shadowOpen = rootDom.attachShadow({ mode: 'open' })
+const root = createRoot(shadowOpen)
+root.render(<App />)
 
-const root = createRoot(document.getElementById('sample'));
-root.render(<h1>React is here!</h1>, root);
+document.addEventListener('selectionchange', () => {
+  const selection = document.getSelection()
+  if (selection == null) {
+    return
+  }
+  const str = selection.toString()
+  console.log('change', str.length, str)
+})
+
+function App() {
+  let [referenceElement, setReferenceElement] = useState()
+  let [popperElement, setPopperElement] = useState()
+  let { styles, attributes } = usePopper(referenceElement, popperElement)
+
+  return (
+    <Popover>
+      {({ open }) => (
+        <>
+          <Popover.Button>Solutions</Popover.Button>
+          {open && (
+            <Popover.Panel
+              ref={setPopperElement}
+              style={styles.popper}
+              {...attributes.popper}
+              static
+            >
+              <p className={popup}>popup content</p>
+            </Popover.Panel>
+          )}
+        </>
+      )}
+    </Popover>
+  )
+}
--- watch.js
+++ watch.js
@@ -1,15 +1,19 @@
-const esbuild = require('esbuild');
+const esbuild = require('esbuild')
 
 const main = async () => {
   const context = await esbuild.context({
-    entryPoints: ['src/background_script.ts', 'src/content_script.tsx', 'src/options_page.tsx'],
+    entryPoints: [
+      'src/background_script.ts',
+      'src/content_script.tsx',
+      'src/options_page.tsx',
+    ],
     outdir: 'dist/src',
     minify: false,
     bundle: true,
     logLevel: 'info',
-  });
+  })
 
-  await context.watch();
-};
+  await context.watch()
+}
 
-main();
+main()
