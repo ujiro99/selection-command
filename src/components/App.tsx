85cc8047330adec878d923855d7ba1f1f5672f05
ujiro99 <siro.cola@gmail.com>
2024-06-19 19:32:24 +0900

    fix: position

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -14,17 +14,14 @@ export const context = createContext<ContextType>({} as ContextType)
 export function App() {
   const [positionElm, setPositionElm] = useState<Element | null>(null)
   const [selectionText, setSelectionText] = useState('')
-  const [rect, setRect] = useState<DOMRect>()
 
   useEffect(() => {
     const onSelectionchange = () => {
       const s = document.getSelection()
       if (s == null || s.rangeCount === 0) {
         setSelectionText('')
-        setRect(undefined)
       } else {
         setSelectionText(s.toString().trim())
-        setRect(s.getRangeAt(0).getBoundingClientRect())
       }
     }
     document.addEventListener('selectionchange', onSelectionchange)
@@ -35,11 +32,7 @@ export function App() {
 
   return (
     <context.Provider value={{ selectionText }}>
-      <SelectAnchor
-        rect={rect}
-        selectionText={selectionText}
-        ref={setPositionElm}
-      />
+      <SelectAnchor selectionText={selectionText} ref={setPositionElm} />
       <Popup positionElm={positionElm} selectionText={selectionText} />
       <OpenInTab />
     </context.Provider>
--- src/components/SelectAnchor.tsx
+++ src/components/SelectAnchor.tsx
@@ -1,33 +1,39 @@
 import React, { useState, useEffect, forwardRef } from 'react'
 import { APP_ID } from '../const'
 
-type Props = {
-  rect: DOMRect | undefined
-  selectionText: string
-}
-
-function isPopup(elm: Element): Boolean {
+function isPopup(elm: Element): boolean {
   if (elm == null) return false
   if (elm.id === APP_ID) return true
   if (elm.nodeName === 'body') return false
   return isPopup(elm.parentElement as Element)
 }
 
+type Point = {
+  x: number
+  y: number
+}
+
+type Props = {
+  selectionText: string
+}
 export const SelectAnchor = forwardRef<HTMLDivElement, Props>(
   (props: Props, ref) => {
-    const rect = props.rect
     const text = props.selectionText
     const selected = text != null && text.length > 0
     const [isMouseDown, setIsMouseDown] = useState(false)
+    const [startPoint, setStartPoint] = useState<Point>({} as Point)
+    const [endPoint, setEndPoint] = useState<Point>({} as Point)
 
     useEffect(() => {
       const onDown = (e: MouseEvent) => {
         if (!isPopup(e.target as Element)) {
           setIsMouseDown(true)
+          setStartPoint({ x: e.x, y: e.y })
         }
       }
-      const onUp = () => {
+      const onUp = (e: MouseEvent) => {
         setIsMouseDown(false)
+        setEndPoint({ x: e.x, y: e.y })
       }
       document.addEventListener('mousedown', onDown)
       document.addEventListener('mouseup', onUp)
@@ -37,18 +43,29 @@ export const SelectAnchor = forwardRef<HTMLDivElement, Props>(
       }
     }, [])
 
-    if (rect == null || isMouseDown || !selected) return null
+    if (isMouseDown || !selected) return null
+
+    const start = { ...startPoint }
+    const end = { ...endPoint }
+    if (startPoint.x > endPoint.x) {
+      start.x = endPoint.x
+      end.x = startPoint.x
+    }
+    if (startPoint.y > endPoint.y) {
+      start.y = endPoint.y
+      end.y = startPoint.y
+    }
 
     const styles = {
       position: 'absolute',
-      top: window.scrollY + rect.top,
-      left: window.scrollX + rect.left,
-      height: rect.height,
-      width: rect.width,
+      top: window.scrollY + start.y,
+      left: window.scrollX + start.x,
+      height: end.y - start.y,
+      width: end.x - start.x,
       pointerEvents: 'none',
       padding: '4px', // adjust position of the Popup
     } as React.CSSProperties
 
-    return <div style={styles} ref={ref}></div>
+    return <div style={styles} ref={ref} />
   },
 )
