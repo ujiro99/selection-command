4dfa5a5b9e965b7508e40d55f1affde88ce19e05
ujiro99 <siro.cola@gmail.com>
2024-06-27 12:30:50 +0900

    add: Enables to copy selected text to the clipboard.

---
+++ src/action/copy.ts
@@ -0,0 +1,22 @@
+import type { ExecProps } from './index'
+import { ExecState } from './index'
+import { sleep } from '@/services/util'
+
+async function setClipboard(text: string) {
+  const type = 'text/plain'
+  const blob = new Blob([text], { type })
+  const data = [new ClipboardItem({ [type]: blob })]
+  await navigator.clipboard.write(data)
+}
+
+export const Copy = {
+  execute({ selectionText, changeState }: ExecProps) {
+    changeState(ExecState.EXECUTING)
+
+    setClipboard(selectionText).then(async () => {
+      changeState(ExecState.SUCCESS)
+      await sleep(500)
+      changeState(ExecState.NONE)
+    })
+  },
+}
--- src/action/index.ts
+++ src/action/index.ts
@@ -3,6 +3,7 @@ import { Popup } from './popup'
 import { Tab } from './tab'
 import { Api } from './api'
 import { LinkPopup } from './linkPopup'
+import { Copy } from './copy'
 import { Option } from './option'
 import type { Command } from '@/services/userSettings'
 
@@ -26,5 +27,6 @@ export const actions = {
   [OPEN_MODE.TAB]: Tab,
   [OPEN_MODE.API]: Api,
   [OPEN_MODE.LINK_POPUP]: LinkPopup,
+  [OPEN_MODE.COPY]: Copy,
   [OPEN_MODE.OPTION]: Option,
 }
