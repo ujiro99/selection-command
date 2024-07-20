229580f63a6d8e5f562606ac22f04d162515dc6e
ujiro99 <siro.cola@gmail.com>
2024-07-20 17:07:29 +0900

    update: Remove safearea and offset.

--- src/components/menu/MenuFolder.tsx
+++ src/components/menu/MenuFolder.tsx
@@ -1,14 +1,12 @@
 import React, { useContext, useState, useEffect, useRef } from 'react'
 import { Popover, PopoverPanel, Transition } from '@headlessui/react'
 import { useFloating, flip, autoUpdate, Placement } from '@floating-ui/react'
-import { offset } from '@floating-ui/dom'
 import classnames from 'classnames'
 
 import { MenuItem } from './MenuItem'
 import { context } from '../App'
 import { Icon } from '@/components/Icon'
 import { STYLE } from '@/const'
-import { sleep } from '@/services/util'
 import type { Command, CommandFolder } from '@/services/userSettings'
 import { useSetting } from '@/hooks/useSetting'
 
@@ -21,54 +19,8 @@ type MenuFolderProps = {
   menuRef: React.RefObject<Element>
 }
 
-const calcSafeAreaHorizontal = (
-  popperElm: Element,
-  folderElm: Element,
-  dataPlacement: Placement,
-) => {
-  const pRect = popperElm.getBoundingClientRect()
-  const mRect = folderElm.getBoundingClientRect()
-  const left = pRect.left - mRect.left
-  const width = pRect.width
-  const height = 12
-  let top = -height
-  if (dataPlacement === 'bottom') {
-    top = mRect.height
-  }
-  return { top, left, width, height }
-}
-
-const calcSafeAreaVertical = (
-  popperElm: Element,
-  folderElm: Element,
-  dataPlacement: Placement,
-) => {
-  const pRect = popperElm.getBoundingClientRect()
-  const mRect = folderElm.getBoundingClientRect()
-  const width = 12
-  const height = pRect.height
-  const top = 0
-  let left = pRect.left - mRect.left - 5
-  if (dataPlacement === 'left') {
-    left = -10
-  }
-  return { top, left, width, height }
-}
-
-const calcSafeArea = (
-  popperElm: Element,
-  folderElm: Element,
-  dataPlacement: Placement,
-) => {
-  if (dataPlacement === 'top' || dataPlacement === 'bottom') {
-    return calcSafeAreaHorizontal(popperElm, folderElm, dataPlacement)
-  }
-  return calcSafeAreaVertical(popperElm, folderElm, dataPlacement)
-}
-
 export function MenuFolder(props: MenuFolderProps): JSX.Element {
   const [visible, setVisible] = useState(false)
-  const [safeAreaStyles, setSafeAreaStyles] = useState<React.CSSProperties>({})
   const folderRef = useRef<HTMLDivElement>(null)
   const { selectionText } = useContext(context)
   const { settings } = useSetting()
@@ -84,7 +36,7 @@ export function MenuFolder(props: MenuFolderProps): JSX.Element {
     placement: popupPlacement,
     elements: { reference: folderRef.current },
     whileElementsMounted: autoUpdate,
-    middleware: [offset(5), flip()],
+    middleware: [flip()],
   })
 
   let dataPlacement = 'right' as Placement
@@ -123,30 +75,6 @@ export function MenuFolder(props: MenuFolderProps): JSX.Element {
     }
   }, [folderRef.current])
 
-  useEffect(() => {
-    ; (async () => {
-      await sleep(50)
-      if (
-        visible &&
-        refs.floating.current != null &&
-        folderRef.current != null
-      ) {
-        const area = calcSafeArea(
-          refs.floating.current,
-          folderRef.current,
-          dataPlacement,
-        )
-        setSafeAreaStyles({
-          position: 'absolute',
-          top: `${area.top}px`,
-          left: `${area.left}px`,
-          width: `${area.width}px`,
-          height: `${area.height}px`,
-        })
-      }
-    })()
-  }, [visible, refs.floating.current, folderRef.current, dataPlacement])
-
   return (
     <Popover
       className={classnames(css.folder, folder, {
@@ -165,7 +93,6 @@ export function MenuFolder(props: MenuFolderProps): JSX.Element {
         {!isHorizontal && <Icon name="chevron" className={css.icon} />}
       </div>
 
-      {visible && <div className="safeArea" style={safeAreaStyles} />}
       <Transition show={visible}>
         <PopoverPanel
           ref={refs.setFloating}
