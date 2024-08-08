46dca224dc6ce18fb1cc315ac76b03138d7556cc
ujiro99 <siro.cola@gmail.com>
2024-08-08 13:52:27 +0900

    fix: The icon should also be displayed in the pop-up on the options screen.

--- src/options_page.tsx
+++ src/options_page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Option } from './components/Option'
+import icons from '../dist/icons.svg'
 
 const dom = document.querySelector('#root')
 if (dom != null) {
   const root = createRoot(dom)
   root.render(<Option />)
+  dom.insertAdjacentHTML('afterend', icons)
 }
