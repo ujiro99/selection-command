08cf3304eca932027f246bb61e4ce88d2aeb5d53
ujiro99 <siro.cola@gmail.com>
2024-12-07 10:01:29 +0900

    test

--- pages/src/app/page.tsx
+++ pages/src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src={`${BASE_PATH}/next.svg`}
+          src={`/next.svg`}
           alt="Next.js logo"
           width={180}
           height={38}
