d8719e36163acb1b63cce6acaa46af7b0812811a
ujiro99 <siro.cola@gmail.com>
2024-12-10 23:02:50 +0900

    update: fix styles.

--- pages/src/components/ListItem.tsx
+++ pages/src/components/ListItem.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { ArrowDownToLine } from 'lucide-react'
 import { Image } from '@/components/Image'
-import { Details } from '@/components/Details'
 import { Badge } from '@/components/ui/badge'
 import type { Command } from '@/types'
 import { cmd2text } from '@/services/util'
@@ -24,26 +23,27 @@ export function ListItem(props: Props): JSX.Element {
             />
             {cmd.title}
           </p>
-          <p className="text-base text-stone-500 text-sm">{cmd.searchUrl}</p>
+          <p className="text-base text-sm text-stone-500">{cmd.searchUrl}</p>
           <p className="text-base">{cmd.description}</p>
         </div>
-        <div className="flex items-center">
-          <Details command={cmd} />
-          <p className="hidden text-stone-500" data-id={cmd.id}>
-            <span className="mx-2 select-none">Installed</span>
-            <span className="p-1 pl-0 select-none">{cmd.download}</span>
+        <div className="flex items-center text-stone-500">
+          <p
+            className="hidden px-2 py-0.5 bg-stone-200 rounded-md"
+            data-id={cmd.id}
+          >
+            <span className="select-none">Installed</span>
           </p>
           <button
-            className="flex items-center text-stone-500 hover:bg-stone-200 rounded"
+            className="hidden hover:bg-stone-200 rounded"
             data-id={cmd.id}
             data-command={cmd2text(cmd)}
           >
             <ArrowDownToLine className="p-1" size={28} />
-            <span className="p-1 pl-0 select-none">{cmd.download}</span>
           </button>
+          <span className="ml-2 p-1 pl-0 select-none">{cmd.download}</span>
         </div>
       </div>
-      <ul className="mt-3 flex gap-2">
+      <ul className="mt-2 flex gap-2">
         {cmd.tags.map((tag) => (
           <li key={tag.id}>
             <Badge className="bg-stone-200 hover:bg-stone-300 text-stone-800 select-none">
