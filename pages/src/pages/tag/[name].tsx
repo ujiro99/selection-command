a284f90a0ec120feaba0afcdd026f2cc3462ebe1
ujiro99 <siro.cola@gmail.com>
2024-12-16 00:52:06 +0900

    add: Enables to export tag pages.

---
+++ pages/src/pages/tag/[name].tsx
@@ -0,0 +1,43 @@
+import type {
+  InferGetStaticPropsType,
+  GetStaticProps,
+  GetStaticPaths,
+} from 'next'
+import { getCommands, getTags } from '@/services/util'
+import Tags from '@/data/tags.json'
+
+type Tag = {
+  name: string
+  count: number
+}
+
+export const getStaticPaths = (async () => {
+  const tags = getTags()
+
+  const paths = tags.map((tag) => ({
+    params: {
+      name: tag.name,
+    },
+  }))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}) satisfies GetStaticPaths
+
+export const getStaticProps = (async ({ params }) => {
+  if (!params) {
+    return { props: { tag: { name: '', count: 0 } } }
+  }
+  const tag = Tags.find((tag) => tag.name === params.name) as Tag
+  return { props: { tag: tag } }
+}) satisfies GetStaticProps<{
+  tag: Tag
+}>
+
+export default function Page({
+  tag,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return tag.count
+}
