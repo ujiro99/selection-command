984d422d70c7c1308aee852c15e77f6b502afaef
ujiro99 <siro.cola@gmail.com>
2024-12-26 23:10:41 +0900

    update: Change output directory for the sitemap.

--- pages/next-sitemap.config.js
+++ pages/next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://ujiro99.github.io/selection-command', // Replace with your site's URL
+  outDir: './out',
   generateRobotsTxt: true, // Optionally generate robots.txt
   transform: async (config, path) => {
     return {
