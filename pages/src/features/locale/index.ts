b4ff29b204a735cb9196a8a534c64dd894a26edd
ujiro99 <siro.cola@gmail.com>
2025-01-07 19:21:17 +0900

    fix: Checks lang is valid.

--- pages/src/features/locale/index.ts
+++ pages/src/features/locale/index.ts
@@ -12,6 +12,13 @@ export const Languages = Object.keys(LanguageMap) as LanguageType[]
 
 export const DefaultLanguage = 'en' as const as LanguageType
 
+const isLang = (lang: any | undefined): lang is LanguageType => {
+  return Languages.includes(lang)
+}
+
 export const getDict = (lang: LanguageType) => {
+  if (!isLang(lang)) {
+    lang = DefaultLanguage as LanguageType
+  }
   return LanguageMap[lang]
 }
