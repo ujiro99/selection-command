f1236d314f256adc20010f86364db6b536025dbd
ujiro99 <siro.cola@gmail.com>
2025-02-23 17:40:16 +0900

    Update: Fix dialog to be portal.

--- src/components/option/Option.tsx
+++ src/components/option/Option.tsx
@@ -1,63 +1,21 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 
 import { Settings } from '@/services/settings'
-import type { SettingsType } from '@/types'
-import { sleep, capitalize, isMenuCommand, isLinkCommand } from '@/lib/utils'
-import { fetchIconUrl } from '@/services/chrome'
-import { APP_ID, VERSION, OPTION_MSG } from '@/const'
+import { capitalize } from '@/lib/utils'
+import { APP_ID, VERSION } from '@/const'
 
 import { Popup } from '@/components/Popup'
 import { TableOfContents } from '@/components/option/TableOfContents'
 import { ImportExport } from '@/components/option/ImportExport'
 import { HubBanner } from '@/components/option/HubBanner'
 import { SettingForm } from '@/components/option/SettingForm'
-import { useEventProxyReceiver } from '@/hooks/option/useEventProxy'
 
 import css from './Option.module.css'
 
 export function Option() {
   const [previewElm, setPreviewElm] = useState<Element | null>(null)
-  const iframeRef = useRef<HTMLIFrameElement>(null)
-  const iframeTORef = useRef<number>(0)
   const [popupElm, setPopupElm] = useState<Element | null>(null)
   const [popupHeight, setPopupHeight] = useState(0)
-
-  useEventProxyReceiver()
-
-  useEffect(() => {
-    const func = async (event: MessageEvent) => {
-      const command = event.data.command
-      const value = event.data.value
-      switch (command) {
-        case OPTION_MSG.START_ACK:
-          clearInterval(iframeTORef.current)
-          break
-        case OPTION_MSG.FETCH_ICON_URL:
-          const { searchUrl } = value
-          console.debug('fetchIconUrl', searchUrl)
-          try {
-            const iconUrl = await fetchIconUrl(searchUrl)
-            sendMessage(OPTION_MSG.RES_FETCH_ICON_URL, { searchUrl, iconUrl })
-          } catch (e) {
-            console.warn('Failed to fetch icon', searchUrl)
-            sendMessage(OPTION_MSG.RES_FETCH_ICON_URL, {
-              searchUrl,
-              iconUrl: null,
-            })
-          }
-          break
-        case OPTION_MSG.OPEN_LINK:
-          window.open(value, '_blank')
-          break
-        default:
-          break
-      }
-    }
-    window.addEventListener('message', func)
-    return () => {
-      window.removeEventListener('message', func)
-    }
-  }, [])
 
   useEffect(() => {
     const updateHeight = () => {
@@ -73,33 +31,6 @@ export function Option() {
       Settings.removeChangedListener(updateHeight)
     }
   }, [popupElm])
-
-  const sendMessage = (command: OPTION_MSG, value: unknown) => {
-    if (iframeRef.current != null && iframeRef.current.contentWindow != null) {
-      const message = { command, value }
-      iframeRef.current.contentWindow.postMessage(message, '*')
-    } else {
-      console.warn('frame null')
-      console.warn(iframeRef)
-    }
-  }
-
-  // const onLoadIframe = async () => {
-  //   const settings = await Settings.get(true)
-  //   const translation = getTranslation()
-
-  //   // Convert linkCommand option
-  //   const linkCommands = settings.commands.filter(isLinkCommand)
-  //   if (linkCommands.length > 0) {
-  //     const linkCommand = linkCommands[0]
-  //     settings.linkCommand = {
-  //       ...settings.linkCommand,
-  //       openMode: linkCommand.openMode,
-  //     }
-  //   }
-  //   settings.commands = settings.commands.filter(isMenuCommand)
-
-  // }
 
   const onClickMenu = (hash: string) => {
     if (!hash) return
--- src/services/defaultSettings.ts
+++ src/services/defaultSettings.ts
@@ -8,6 +8,7 @@ import {
   LINK_COMMAND_ENABLED,
   LINK_COMMAND_STARTUP_METHOD,
   KEYBOARD,
+  SPACE_ENCODING,
 } from '@/const'
 
 export default {
@@ -76,17 +77,20 @@ export const DefaultCommands = [
     id: '723ee9f1-150b-54ad-aeac-5fbfd0ae3650',
     iconUrl: 'https://www.google.com/favicon.ico',
     openMode: OPEN_MODE.POPUP,
+    openModeSecondary: OPEN_MODE.TAB,
     searchUrl: 'https://google.com/search?q=%s',
     title: 'Google',
     popupOption: {
       width: PopupOption.width,
       height: PopupOption.height,
     },
+    spaceEncoding: SPACE_ENCODING.PLUS,
   },
   {
     id: '18998bac-4e6c-503f-b3ea-b9198edef7c3',
     iconUrl: 'https://www.google.com/favicon.ico',
     openMode: OPEN_MODE.POPUP,
+    openModeSecondary: OPEN_MODE.TAB,
     searchUrl: 'https://google.com/search?q=%s&tbm=isch',
     title: 'Google Image',
     parentFolderId: '222d6489-4eca-48fd-8590-fceb30545bab',
@@ -94,56 +98,93 @@ export const DefaultCommands = [
       width: PopupOption.width,
       height: PopupOption.height,
     },
+    spaceEncoding: SPACE_ENCODING.PLUS,
   },
   {
     id: '92f8f4e8-3cd8-4e01-9908-811d72448670',
     iconUrl: 'https://www.amazon.com/favicon.ico',
     openMode: OPEN_MODE.TAB,
+    openModeSecondary: OPEN_MODE.TAB,
     searchUrl: 'https://www.amazon.com/s?k=%s',
     title: 'Amazon',
     parentFolderId: '222d6489-4eca-48fd-8590-fceb30545bab',
+    popupOption: {
+      width: PopupOption.width,
+      height: PopupOption.height,
+    },
+    spaceEncoding: SPACE_ENCODING.PLUS,
   },
   {
     id: '2e0cd6fe-a232-402e-846c-2104f0639995',
     iconUrl: 'https://www.youtube.com/s/desktop/f574e7a2/img/favicon_32x32.png',
     openMode: OPEN_MODE.TAB,
+    openModeSecondary: OPEN_MODE.TAB,
     searchUrl: 'https://www.youtube.com/results?search_query=%s',
     title: 'Youtube',
     parentFolderId: 'a3495269-0a4d-4866-a519-bca75ed1c246',
+    popupOption: {
+      width: PopupOption.width,
+      height: PopupOption.height,
+    },
+    spaceEncoding: SPACE_ENCODING.PLUS,
   },
   {
     id: '409aba7d-c49e-5e81-b8a6-a862f2651b3e',
     iconUrl:
       'https://assets.nflxext.com/ffe/siteui/common/icons/nficon2016.ico',
     openMode: OPEN_MODE.TAB,
+    openModeSecondary: OPEN_MODE.TAB,
     searchUrl: 'https://www.netflix.com/search?q=%s',
     title: 'Netflix',
     parentFolderId: 'a3495269-0a4d-4866-a519-bca75ed1c246',
+    popupOption: {
+      width: PopupOption.width,
+      height: PopupOption.height,
+    },
+    spaceEncoding: SPACE_ENCODING.PLUS,
   },
   {
     id: '54f3e785-960a-5a70-9c80-cfbf0357c4c7',
     iconUrl: 'https://s.pinimg.com/webapp/favicon-22eb868c.png',
     openMode: OPEN_MODE.TAB,
+    openModeSecondary: OPEN_MODE.TAB,
     searchUrl: 'https://www.pinterest.com/search/pins/?q=%s',
     title: 'Pinterest',
     parentFolderId: 'a3495269-0a4d-4866-a519-bca75ed1c246',
+    popupOption: {
+      width: PopupOption.width,
+      height: PopupOption.height,
+    },
+    spaceEncoding: SPACE_ENCODING.PLUS,
   },
   {
     id: 'bbf701ee-6443-49d8-b9c0-3acfa8034c5d',
     iconUrl:
       'https://ssl.gstatic.com/docs/doclist/images/drive_2022q3_32dp.png',
     openMode: OPEN_MODE.TAB,
+    openModeSecondary: OPEN_MODE.TAB,
     searchUrl: 'https://drive.google.com/drive/search?q=%s',
     title: 'Drive',
     parentFolderId: '01710cf1-ec8b-497f-8d1f-9cb716567bc4',
+    popupOption: {
+      width: PopupOption.width,
+      height: PopupOption.height,
+    },
+    spaceEncoding: SPACE_ENCODING.PLUS,
   },
   {
     id: '6c6d45d6-735b-43bd-b4c8-5dc5104c23ed',
     iconUrl: 'https://ssl.gstatic.com/translate/favicon.ico',
     openMode: OPEN_MODE.TAB,
+    openModeSecondary: OPEN_MODE.TAB,
     searchUrl:
       'https://translate.google.co.jp/?hl=ja&sl=auto&text=%s&op=translate',
     title: 'en to ja',
     parentFolderId: '01710cf1-ec8b-497f-8d1f-9cb716567bc4',
+    popupOption: {
+      width: PopupOption.width,
+      height: PopupOption.height,
+    },
+    spaceEncoding: SPACE_ENCODING.PLUS,
   },
 ] as Command[]
