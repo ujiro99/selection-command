2e5229efd4a7146342f61e67a1da03456c394de8
ujiro99 <siro.cola@gmail.com>
2025-03-23 14:37:55 +0900

    Fix: Don't use DynamicIcon.

--- src/components/pageAction/TypeIcon.tsx
+++ src/components/pageAction/TypeIcon.tsx
@@ -1,6 +1,13 @@
-import { DynamicIcon } from 'lucide-react/dynamic'
 import { PAGE_ACTION_EVENT, PAGE_ACTION_CONTROL } from '@/const'
-import { actionToLucide } from '@/lib/utils'
+import {
+  ArrowDownFromLine,
+  ArrowDownToLine,
+  MousePointerClick,
+  Keyboard,
+  Type,
+  Mouse,
+  CircleHelp,
+} from 'lucide-react'
 
 export const TypeIcon = ({
   type,
@@ -11,11 +18,22 @@ export const TypeIcon = ({
   size?: number
   className?: string
 }) => {
-  return (
-    <DynamicIcon
-      className={className}
-      size={size}
-      name={actionToLucide(type)}
-    />
-  )
+  switch (type) {
+    case 'start':
+      return <ArrowDownFromLine size={size} className={className} />
+    case 'end':
+      return <ArrowDownToLine size={size} className={className} />
+    case 'click':
+    case 'doubleClick':
+    case 'tripleClick':
+      return <MousePointerClick size={size} className={className} />
+    case 'keyboard':
+      return <Keyboard size={size} className={className} />
+    case 'input':
+      return <Type size={size} className={className} />
+    case 'scroll':
+      return <Mouse size={size} className={className} />
+    default:
+      return <CircleHelp size={size} className={className} />
+  }
 }
