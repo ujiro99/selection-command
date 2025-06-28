2aae4893ab93c52fbd43391ca35ea0db07bec9cb
ujiro99 <siro.cola@gmail.com>
2025-06-28 16:34:20 +0900

    Update: Fix hover area.

--- src/components/menu/HoverArea.tsx
+++ src/components/menu/HoverArea.tsx
@@ -4,6 +4,8 @@ type Props = {
   isHorizontal: boolean
 }
 
+type Placement = 'top' | 'bottom' | 'left' | 'right'
+
 export const HoverArea = (props: Props) => {
   const { anchor, content, isHorizontal } = props
 
@@ -21,55 +23,62 @@ export const HoverArea = (props: Props) => {
   const isTop = isHorizontal && anchor.y <= content.y
   const isBottom = isHorizontal && anchor.bottom >= content.bottom
   const isLeft = !isHorizontal && anchor.x <= content.x
-  const isRight = !isHorizontal && anchor.right >= content.right
+  const placement: Placement = isTop
+    ? 'top'
+    : isBottom
+      ? 'bottom'
+      : isLeft
+        ? 'left'
+        : 'right'
 
   let top = 0
   let left = 0
   let d
 
-  if (isTop) {
-    d = `M ${anchor.right} ${anchor.top}
+  switch (placement) {
+    case 'top':
+      d = `M ${anchor.right} ${anchor.top}
          Q ${anchor.right} ${content.top},
            ${content.right} ${content.top}
          h ${-content.width}
          Q ${anchor.x} ${content.top},
            ${anchor.x} ${anchor.top}
-         v ${anchor.height}
          h ${anchor.width}
          z`
-    top = anchor.top - content.top
-  } else if (isBottom) {
-    d = `M ${anchor.left} ${anchor.bottom}
+      top = anchor.top - content.top
+      break
+    case 'bottom':
+      d = `M ${anchor.left} ${anchor.bottom}
          Q ${anchor.left} ${content.bottom},
            ${content.left} ${content.bottom}
          h ${content.width}
          Q ${anchor.right} ${content.bottom},
            ${anchor.right} ${anchor.bottom}
-         v ${-anchor.height}
          h ${-anchor.width}
          z`
-  } else if (isLeft) {
-    d = `M ${anchor.x} ${anchor.y}
+      break
+    case 'left':
+      d = `M ${anchor.x} ${anchor.y}
          Q ${content.x} ${anchor.y},
            ${content.x} ${content.y}
          v ${content.height}
          Q ${content.x} ${anchor.bottom},
            ${anchor.x} ${anchor.bottom}
-         h ${anchor.width}
          v ${-anchor.height}
          z`
-    left = -anchor.width + 2
-  } else if (isRight) {
-    d = `M ${anchor.right} ${anchor.top}
+      left = -anchor.width + 2
+      break
+    case 'right':
+      d = `M ${anchor.right} ${anchor.top}
          Q ${content.right} ${anchor.top},
            ${content.right} ${content.top}
          v ${content.height}
          Q ${content.right} ${anchor.bottom},
            ${anchor.right} ${anchor.bottom}
-         h ${-anchor.width}
          v ${-anchor.height}
          z`
-    left = -2
+      left = -2
+      break
   }
 
   return (
