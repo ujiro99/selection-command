342e01f1a9adfdbeb4b5c2968bb690b85526a311
ujiro99 <siro.cola@gmail.com>
2025-08-03 11:44:58 +0900

    Fix: tsc error.

--- src/components/option/editor/CommandEditDialog.tsx
+++ src/components/option/editor/CommandEditDialog.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react"
+import { useState, useEffect } from "react"
 import { useForm, useFieldArray, useWatch } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import {
