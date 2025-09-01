aabbc3fcc95559b28a248982405ccc18689d2c3c
ujiro99 <siro.cola@gmail.com>
2025-09-01 22:55:15 +0900

    Fix: unit tests.

--- packages/hub/src/features/command.actual.test.ts
+++ packages/hub/src/features/command.actual.test.ts
@@ -4,6 +4,10 @@ import { cmd2uuid, getCommands } from "./command"
 describe("Command UUID Generation", () => {
   test("CH-15: Check current ids", async () => {
     for (const cmd of getCommands()) {
+      if (cmd.revision > 0) {
+        // Skip commands that have been revised
+        continue
+      }
       const uuid = cmd2uuid(cmd)
       expect(uuid, `${cmd.title}`).toMatch(cmd.id)
     }
