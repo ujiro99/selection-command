21c45beb5cab16a68a97e6c1cecb941a483e2099
ujiro99 <siro.cola@gmail.com>
2026-02-21 14:59:18 +0900

    Fix: Fix issue where clipboard is unavailable in PageAction

--- packages/extension/src/action/pageAction.ts
+++ packages/extension/src/action/pageAction.ts
@@ -1,10 +1,11 @@
 import { Ipc, BgCommand } from "@/services/ipc"
 import { getScreenSize, getWindowPosition } from "@/services/screen"
 import { isValidString, isPageActionCommand } from "@/lib/utils"
-import { PAGE_ACTION_OPEN_MODE } from "@/const"
+import { PAGE_ACTION_OPEN_MODE, PAGE_ACTION_EVENT } from "@/const"
 import { PopupOption } from "@/services/option/defaultSettings"
 import type { ExecuteCommandParams, UrlParam } from "@/types"
 import type { OpenAndRunProps } from "@/services/pageAction/background"
+import { INSERT, InsertSymbol } from "@/services/pageAction"
 
 type PageActionParams = {
   userVariables?: Array<{ name: string; value: string }>
@@ -33,10 +34,18 @@ export const PageAction = {
       return
     }
 
+    // Checks if any step requires clipboard data
+    const needClipboard = command.pageActionOption.steps.some((step) => {
+      return (
+        step.param.type === PAGE_ACTION_EVENT.input &&
+        step.param.value.includes(InsertSymbol[INSERT.CLIPBOARD])
+      )
+    })
+
     const url: UrlParam = {
       searchUrl: command.pageActionOption.startUrl,
       selectionText,
-      useClipboard: useClipboard ?? false,
+      useClipboard: needClipboard || useClipboard,
     }
 
     const openMode = useSecondary
