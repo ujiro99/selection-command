ce4ef4a18d4ca63a709f02a505ecdf7a5a7b2296
ujiro99 <siro.cola@gmail.com>
2026-03-15 06:05:52 +0900

    Fix: e2e.

--- packages/extension/e2e/fixtures.ts
+++ packages/extension/e2e/fixtures.ts
@@ -1,6 +1,8 @@
 import { test as base, chromium, type BrowserContext } from "@playwright/test"
 import path from "path"
+import { fileURLToPath } from "url"
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const pathToExtension = path.join(__dirname, "../dist")
 
 /**
